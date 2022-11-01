f4e324497929f70307ec779835f77890307dc4b0
DeveloppementWeb19 <pierre-louis.clavel@laposte.net>
2022-11-01 17:25:42 +0100

    Second commit

--- CrazyEight/src/main/java/game/Card.java
+++ CrazyEight/src/main/java/game/Card.java
@@ -1,20 +1,20 @@
 package game;
 
 public class Card {
-    private String name; //attribut name de la carte
-    private String color; //attribut color de la carte
+    private String name; //name of the card
+    private String color; //color of the card
     
     protected Card(String name, String color){ //constructeur qui récupère les informations données par les paramètres
         this.name = name; 
         this.color = color;
     }
 
-    protected static String getName(Card carte){
-        return carte.name;
+    protected static String getName(Card card){
+        return card.name;
     }
 
-    protected static String getColor(Card carte){
-        return carte.color;
+    protected static String getColor(Card card){
+        return card.color;
     }
 
     @Override
--- CrazyEight/src/main/java/game/Deck.java
+++ CrazyEight/src/main/java/game/Deck.java
@@ -3,37 +3,43 @@ import java.util.LinkedList;
 import java.util.Random;
 import java.util.Queue;
 import java.util.PriorityQueue;
+import java.util.stream.*;
+import java.util.Arrays;
 
 
 
 public class Deck {
 
-    //protected static LinkedList<Card> pioche = new LinkedList<Card>();
-    private final String [] names = {"ACE", "KING", "QUEEN", "JACK", "TEN", "NINE", "EIGHT", "SEVEN","SIX", "FIVE", "FOUR", "THREE", "TWO"}; // les 8 valeurs constantes
-    private final String [] colors = {"HEARTS", "DIAMONDS", "CLUBS", "SPADES"}; // les 4 couleurs constantes
+    //protected Queue<Card> deck = deckShuffledAsQueue(deckGenerator());
+    private static final String [] names = {"ACE", "KING", "QUEEN", "JACK", "TEN", "NINE", "EIGHT", "SEVEN","SIindex", "FIVE", "FOUR", "THREE", "TWO"}; // les 8 valeurs constantes
+    private static final String [] colors = {"HEARTS", "DIAMONDS", "CLUBS", "SPADES"}; // les 4 couleurs constantes
     private static Random random = new Random();
+
     
-    protected LinkedList<Card> deckCreation(){ // création des 32 cartes et ne retourne rien
-        LinkedList<Card> pioche = new LinkedList<Card>();
+    
+    
+    protected static LinkedList<Card> deckGenerator(){ // création des 32 cartes et ne retourne rien
+        LinkedList<Card> deck = new LinkedList<Card>();
         for(String nom : names){ // parcours des valeurs constantes
             for(String couleur : colors){ 
                 Card carte = new Card(nom, couleur); // appel à la classe Card pour créer un objet Carte
-                pioche.add(carte); // et l'ajouter au deck
+                deck.add(carte); // et l'ajouter au deck
             }
         }
-        return pioche;
+        return deck;
     }
 
     
-    protected Queue<Card> ArrayToQueue(LinkedList<Card> deck){
-        Queue<Card> finalDeck = new PriorityQueue<Card>(); //empty
+    protected static PriorityQueue<Card> deckShuffledAsQueue(LinkedList<Card> deck){
+        PriorityQueue<Card> finalDeck = new PriorityQueue<Card>(); //empty
         while(!deck.isEmpty()){
-            int x = random.nextInt(deck.size()-1);
-            finalDeck.add(deck.get(x));
-            deck.remove(x);
+            int index = random.nextInt(deck.size()-1);
+            finalDeck.add(deck.get(index));
+            deck.remove(index);
         } 
         return finalDeck;
     }
+
 
    
 
